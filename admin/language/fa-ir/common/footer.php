8adde4be8cb0d4b9ecadff57199b23e405c6bbda
Babak Ahari <babak.ahari@gmail.com>
2021-02-10 00:00:41 +0100

    trying to upload

---
+++ admin/language/fa-ir/common/footer.php
@@ -0,0 +1,5 @@
+<?php
+// Text
+$_['text_footer'] 	= 'Powered By OpenCart <a href="http://www.opencart.com">OpenCart.Com</a> &copy; ' . date('Y') . ' All Rights Reserved';
+$_['text_version'] 	= 'Version %s';
+?>
